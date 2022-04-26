692e332ec20c8098084ca79d07f745c7a807f23f
Miskannn <maksim.kovtun2103@gmail.com>
2022-04-26 16:14:02 +0300

    last fix

--- src/medium/37-PromiseAll.ts
+++ src/medium/37-PromiseAll.ts
@@ -1,3 +1,5 @@
+import {Equal} from "@type-challenges/utils";
+
 declare function PromiseAll<T extends unknown[]>(values: readonly [...T])
     :Promise<{[P in keyof T] : (T[P] extends Promise<infer R> ? R : T[P]) }>
 
@@ -8,8 +10,11 @@ const promise3 = new Promise<string>((resolve, reject) => {
 });
 
 // expected to be `Promise<[number, 42, string]>`
-const p: Promise<[number, 42, string]> = PromiseAll([promise1, promise2, promise3] as const);
+const p = PromiseAll([promise1, promise2, promise3] as const);
 
+type testType = Promise<[number, 42, string]>
+
+type test = Equal<typeof p, testType>
 
 /**
  * Link to challenge: https://github.com/type-challenges/type-challenges/blob/main/questions/00020-medium-promise-all/README.md
